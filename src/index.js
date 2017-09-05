1fe532c32f2a7f4c4c65e82bb81730538527ffc6
Ian T. Small <ian.small@thekeepstudios.com>
2017-09-05 13:31:07 -0400

    run prettier on file

--- src/index.js
+++ src/index.js
@@ -1,44 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Hex, { gridPoints } from 'react-hex';
+import Hex, { gridPoints } from "react-hex";
 
 class Board extends React.Component {
-
-    getType(){
-        let {type} = this.props;
-        return (type === undefined) ? 'pointy-topped' : type;
+    getType() {
+        let { type } = this.props;
+        return type === undefined ? "pointy-topped" : type;
     }
 
-    getSize(){
-        let {size} = this.props;
-        return (size === undefined) ? 10 : size;
+    getSize() {
+        let { size } = this.props;
+        return size === undefined ? 10 : size;
     }
 
-    getOffsetX(){
-        let {oX} = this.props;
-        return (oX === undefined) ? 100 : oX;
+    getOffsetX() {
+        let { oX } = this.props;
+        return oX === undefined ? 100 : oX;
     }
 
-    getOffsetY(){
-        let {oY} = this.props;
-        return (oY === undefined) ? 100 : oY;
+    getOffsetY() {
+        let { oY } = this.props;
+        return oY === undefined ? 100 : oY;
     }
 
-    getWidth(){
-        let {width} = this.props;
-        return (width === undefined) ? 25 : width;
+    getWidth() {
+        let { width } = this.props;
+        return width === undefined ? 25 : width;
     }
 
-    getHeight(){
-        let {height} = this.props;
-        return (height === undefined) ? 25 : height;
+    getHeight() {
+        let { height } = this.props;
+        return height === undefined ? 25 : height;
     }
 
-    getOnClick(){
-        let {onClick} = this.props;
-        return (onClick === undefined)
-            ? ((i) => {console.log("No onClick defined, click unhandled");})
+    getOnClick() {
+        let { onClick } = this.props;
+        return onClick === undefined
+            ? i => {
+                  console.log("No onClick defined, click unhandled");
+              }
             : onClick;
     }
 
@@ -55,15 +56,27 @@ class Board extends React.Component {
         const points = gridPoints(type, oX, oY, size, width, height);
 
         const hexes = points.map((point, i) => {
-            const {props} = point;
+            const { props } = point;
             const key = i;
             const owner = board.props.hexes[key];
             const fill = owner === undefined ? "white" : owner;
-            return (<Hex key={key} {...props} fill={fill} stroke="grey" onClick={() => onClick(key)}/>);
+            return (
+                <Hex
+                    key={key}
+                    {...props}
+                    fill={fill}
+                    stroke="grey"
+                    onClick={() => onClick(key)}
+                />
+            );
         });
 
         //return the gameboard containing hexes
-        return ( <svg width="100%" height="100%">{hexes}</svg>);
+        return (
+            <svg width="100%" height="100%">
+                {hexes}
+            </svg>
+        );
     }
 }
 
@@ -88,9 +101,7 @@ class Game extends React.Component {
         if (calculateWinner(hexes) || hexes[i]) {
             return;
         }
-        hexes[i] = this.state.xIsNext
-            ? "red"
-            : "blue";
+        hexes[i] = this.state.xIsNext ? "red" : "blue";
         this.setState({
             history: history.concat([
                 {
@@ -115,14 +126,10 @@ class Game extends React.Component {
         const winner = calculateWinner(current.hexes);
 
         const moves = history.map((step, move) => {
-            const desc = move
-                ? "Move #" + move
-                : "Game start";
+            const desc = move ? "Move #" + move : "Game start";
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>
-                        {desc}
-                    </button>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
         });
@@ -131,9 +138,7 @@ class Game extends React.Component {
         if (winner) {
             status = "Winner: " + winner;
         } else {
-            status = "Next player: " + (this.state.xIsNext
-                ? "Red"
-                : "Blue");
+            status = "Next player: " + (this.state.xIsNext ? "Red" : "Blue");
         }
 
         return (
@@ -147,7 +152,8 @@ class Game extends React.Component {
                         oX={10}
                         oY={10}
                         hexes={current.hexes}
-                        onClick={key => this.handleClick(key)}/>
+                        onClick={key => this.handleClick(key)}
+                    />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
@@ -160,8 +166,7 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-    <Game/>, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(hexes) {
     //TODO calculate winner of HexTile game
