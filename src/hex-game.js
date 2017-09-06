2043532bfbc94a5c9181815026a55e053150569c
Ian T. Small <ian.small@thekeepstudios.com>
2017-09-06 16:19:50 -0400

    Finished win condition pathfinding checker

--- src/hex-game.js
+++ src/hex-game.js
@@ -3,30 +3,81 @@ import HexBoard from "./hex-board.js";
 import {convertArrayToMatrix} from "./utils.js";
 import {gridPoints} from "react-hex";
 
-const calculateWinner = (hexes) => {
-    function findPath(origin, desination, hexes) {
-        const originType = hexes[origin];
-        const destinationType = hexes[desination];
-        if (originType !== destinationType) {
-            return []; //return an empty path, as these are not matching hexes
-        }
-        //create matrix of hexes
-        const bsize = Math.sqrt(hexes.length); //store board width/height size;
-        //make new hexArray containing grid data (ensures filled in array)
-        const hexArray = gridPoints("pointy-topped", 1, 1, 1, bsize, bsize).map((point, index) => {
-            return {owner: hexes[index], x: point.gridX, y: point.gridY};
-        });
-        //convert array to matrix
-        const hexGrid = convertArrayToMatrix(hexArray, bsize);
-        //TODO implement simple depth first search for path in matrix of hexes
-        const path = [];
-        return path;
-    }
-    //TODO calculate winner of HexTile game
-    const path = findPath(0, hexes.length - 1, hexes);
-    return path !== undefined && path.length > 0;
-}
+const getNeighbors = (hex, matrix) => {
+    const getHex = (x, y) => {
+        return matrix[y] !== undefined
+            ? matrix[y][x]
+            : undefined
+    };
+    const upLeft = getHex(hex.x, hex.y - 1);
+    const upRight = getHex(hex.x + 1, hex.y - 1);
+    const left = getHex(hex.x - 1, hex.y);
+    const right = getHex(hex.x + 1, hex.y);
+    const downLeft = getHex(hex.x - 1, hex.y + 1);
+    const downRight = getHex(hex.x, hex.y + 1);
+    return [
+        upLeft,
+        upRight,
+        right,
+        downRight,
+        downLeft,
+        left
+    ];
+};
 
+const findPath = (origin, desination, hexes) => {
+    //convert raw hexes into more useful matrix storage
+    const originType = hexes[origin];
+    const destinationType = hexes[desination];
+    if (originType !== destinationType) {
+        return false; //return false, as these are not matching hexes
+    }
+    //store board width/height size (calculation not cheap)
+    const bsize = Math.sqrt(hexes.length);
+    //make new hexArray containing grid data (ensures filled in array)
+    const hexArray = gridPoints("pointy-topped", 1, 1, 1, bsize, bsize).map((point, index) => {
+        return {owner: hexes[index], x: point.gridX, y: point.gridY, visited: false};
+    });
+    //get origin and desination hexes
+    const originHex = hexArray[origin];
+    const destinationHex = hexArray[desination];
+    //convert array to matrix
+    const hexGrid = convertArrayToMatrix(hexArray, bsize);
+    //simple breadth first search for path in matrix of hexes
+    const toVisit = [originHex]; //hexes to visit
+    //while toVisit is not empty visit the next hex.
+    console.log("Starting path finding " + originHex.owner);
+    for (let nextHex = toVisit.shift(); nextHex !== undefined; nextHex = toVisit.shift()) {
+        console.log("    Visting" + JSON.stringify(nextHex));
+        nextHex.visited = true;
+        //check if next hex is the desintation, return true if yes
+        if (nextHex === destinationHex) {
+            return true; //we've found our destination, we can return true
+        }
+        // add neighbors to toVisit IF they have the same owner AND they haven't already been visited
+        const neighbors = getNeighbors(nextHex, hexGrid);
+        for (let nextNeighbor = neighbors.shift(); neighbors.length > 0; nextNeighbor = neighbors.shift()) {
+            const isVisitable = nextNeighbor !== undefined && nextNeighbor.owner === nextHex.owner && !nextNeighbor.visited;
+            if (isVisitable) {
+                console.log("        Adding" + JSON.stringify(nextNeighbor));
+                toVisit.push(nextNeighbor);
+            }
+        }
+    }
+    console.log("Finished path finding");
+    return false;
+};
+
+const calculateWinner = (hexes) => {
+    //calculate winner of HexTile game by determining which has a winning path
+    const isblueWinner = findPath(0, hexes.length - 1, hexes);
+    const isRedWinner = !isblueWinner && findPath(1, hexes.length - 2, hexes);
+    if (isblueWinner) {
+        return "blue";
+    } else if (isRedWinner) {
+        return "red";
+    }
+};
 
 class HexGame extends React.Component {
     constructor(props) {
@@ -60,7 +111,8 @@ class HexGame extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const hexes = Array.from(current.hexes);
-        if (calculateWinner(hexes) || hexes[i]) {
+        //check if this hex is alredy defined OR if there is already a winner
+        if (hexes[i] || calculateWinner(hexes)) {
             return;
         }
         hexes[i] = this.state.xIsNext
