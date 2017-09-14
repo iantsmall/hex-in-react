a5b62d73329cdad06af04bafd799291f61bd18fa
Ian T. Small <ian.small@thekeepstudios.com>
2017-09-14 16:13:26 -0400

    Added some site design to the app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import {Grid, Row, Col, Jumbotron, Nav, NavItem} from 'react-bootstrap';
+import "./app.css";
+import {
+    Grid,
+    Row,
+    Col,
+    Jumbotron,
+    Navbar,
+    Nav,
+    NavItem
+} from 'react-bootstrap';
 import HexGame from "./hex-game.js";
 import Rules from "./rules.js";
 import ComingSoonModal from "./coming-soon-modal.js";
@@ -13,9 +21,9 @@ class App extends React.Component {
     render() {
 
         //define child components
-        const hexGame = (<HexGame boardSize={9} ref={(childRef) => { this.board = childRef; }}/>);
-        const rulesModal = (<Rules boardSize={9} ref={(childRef) => { this.rulesModal = childRef; }}/>);
-        const comingSoonModal = ( <ComingSoonModal ref={(childRef) => { this.comingSoonModal = childRef; }}/> );
+        const hexGame = (<HexGame boardSize={9} ref={(childRef) => {this.board = childRef;}}/>);
+        const rulesModal = (<Rules boardSize={9} ref={(childRef) => {this.rulesModal = childRef;}}/>);
+        const comingSoonModal = (<ComingSoonModal ref={(childRef) => {this.comingSoonModal = childRef;}}/>);
 
         const handleRulesClick = (() => {
             this.rulesModal.open();
@@ -29,32 +37,45 @@ class App extends React.Component {
             navFunction();
         });
 
+        const navbarInstance = (
+            <Navbar inverse collapseOnSelect onSelect={handleSelect}>
+                <Navbar.Header>
+                    <Navbar.Brand><a href="#">Hex in React</a></Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <Navbar.Text>
+                            A "React in Every Framework" Project
+                        </Navbar.Text>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={handleRulesClick} title="Item">Game Rules</NavItem>
+                        <NavItem eventKey={handleComingSoonClick} title="Item">Coming Soon</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+
         return (
-            <Grid id="app">
-                <Row>
-                    <Col xs={12}>
-                        <div id="alert"></div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
-                        <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
-                            {/* <NavItem eventKey={handleHexClick} href="/home">Hex in React</NavItem> */}
-                            <NavItem eventKey={handleRulesClick} title="Item">Game Rules</NavItem>
-                            <NavItem eventKey={handleComingSoonClick} title="Item">Coming Soon</NavItem>
-                        </Nav>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
-                        <Jumbotron id="viewport" className="modal-container">
-                            <HexGame boardSize={9}/>
-                            {comingSoonModal}
-                            {rulesModal}
-                        </Jumbotron>
-                    </Col>
-                </Row>
-            </Grid>
+            <div id="app">
+                {navbarInstance}
+                <Grid>
+                    <Row>
+                        <Col xs={12}>
+                            <div id="alert"></div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <Jumbotron id="viewport" className="modal-container">
+                                <HexGame boardSize={9}/> {comingSoonModal}
+                                {rulesModal}
+                            </Jumbotron>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
         )
     }
 }
